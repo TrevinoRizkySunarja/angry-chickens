64e0482752c9021325de18711887a08e78d1047e
TrevinoRizkySunarja <trevprive123@gmail.com>
2025-05-28 13:24:49 +0200

    Test

--- src/js/chicken.js
+++ src/js/chicken.js
@@ -1,18 +1,37 @@
-import { Actor, Keys, Vector, CollisionType } from "excalibur"
-import { Resources } from './resources'
+import { Actor, Keys, Vector, CollisionType } from "excalibur";
+import { Resources } from './resources.js';
 
 export class Chicken extends Actor {
-
     constructor() {
-        super({ width: Resources.Bird.width, height: Resources.Bird.height })
-        this.pos = new Vector(200, 360)
+        super({ 
+            width: Resources.Bird.width, 
+            height: Resources.Bird.height,
+            collisionType: CollisionType.Active
+        });
+        this.pos = new Vector(200, 360);
+        this.launched = false;
     }
+
     onInitialize(engine) {
-        this.graphics.use(Resources.Bird.toSprite())
+        this.graphics.use(Resources.Bird.toSprite());
     }
+
     onPreUpdate(engine) {
-        if (engine.input.keyboard.wasPressed(Keys.Space)) {
-            Resources.ChickenOne.play()
+        if (engine.input.keyboard.wasPressed(Keys.Space) && !this.launched) {
+            Resources.ChickenOne.play();
+            this.vel = new Vector(400, -400); // Launch speed and direction
+            this.launched = true;
         }
+
+        if (this.pos.y > engine.drawHeight || this.pos.x > engine.drawWidth) {
+            // Reset chicken position after leaving the screen
+            this.resetPosition();
+        }
+    }
+
+    resetPosition() {
+        this.pos = new Vector(200, 360);
+        this.vel = Vector.Zero;
+        this.launched = false;
     }
 }
--- src/js/tree.js
+++ src/js/tree.js
@@ -1,14 +1,25 @@
-import { Actor, Engine, Vector, CollisionType } from "excalibur"
+import { Actor, Vector, CollisionType } from "excalibur"
 import { Resources } from './resources'
-import { Chicken } from './chicken'
 
 export class Tree extends Actor {
     constructor(x, y, degrees) {
-        super({x,y,width:Resources.Tree.width, height:Resources.Tree.height})
+        super({
+            x, y, 
+            width: Resources.Tree.width, 
+            height: Resources.Tree.height,
+            collisionType: CollisionType.Fixed
+        })
         this.graphics.use(Resources.Tree.toSprite())
-
-        const radians = (degrees * Math.PI) / 180;
-        this.rotation = radians
+        this.rotation = (degrees * Math.PI) / 180
+        this.health = 3
     }
 
+    takeDamage() {
+        this.health -= 1
+        this.graphics.opacity = this.health / 3
+
+        if (this.health <= 0) {
+            this.kill()
+        }
+    }
 }
