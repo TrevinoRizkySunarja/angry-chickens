af210a96bfce9712dad3415cb2e74b69f1daad79
TrevinoRizkySunarja <trevprive123@gmail.com>
2025-05-28 13:21:43 +0200

    Test

--- src/js/game.js
+++ src/js/game.js
@@ -10,7 +10,7 @@ export class Game extends Engine {
 
     constructor() {
         super({ 
-            width: 1000, 
+            width: 1200, 
             height: 600, 
             maxFps: 60, 
             displayMode: DisplayMode.FitScreen,
@@ -28,8 +28,16 @@ export class Game extends Engine {
 
         this.add(new Floor())
 
-        this.add(new Tree(650, 234, 0))
-        this.add(new Tree(650, 340, 90))
+// Left wall
+this.add(new Tree(570, 340, 90));    // Left wall
+this.add(new Tree(730, 340, 90));    // Right wall
+this.add(new Tree(650, 235, 0));     // Roof base
+this.add(new Tree(570, 130, 90));   // Left roof slope
+this.add(new Tree(730, 130, 90));    // Right roof slope
+this.add(new Tree(650, 25, 0));     // Roof base
+
+
+
 
         this.add(new Chicken())
     }
